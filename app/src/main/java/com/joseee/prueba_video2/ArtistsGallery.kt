0e060594b09daaec2f7d9d3156bc98660f905e47
navad <navadiego800@gmail.com>
2025-05-12 12:20:55 -0600

    Galeria de imagenes

--- app/src/main/java/com/joseee/prueba_video2/ArtistsGallery.kt
+++ app/src/main/java/com/joseee/prueba_video2/ArtistsGallery.kt
@@ -1,10 +1,79 @@
 package com.joseee.prueba_video2
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
-fun ArtistsGallery(){
+fun ArtistsGallery(navController: NavController) {
+    val images = listOf(
+        R.drawable.ic_launcher_background, // Imagen 1
+        R.drawable.ic_launcher_foreground, // Imagen 2
+        R.drawable.ic_launcher_background, // Imagen 3
+        R.drawable.ic_launcher_foreground  // Imagen 4
+    )
 
-    //Acá va el codigo
+    val routes = listOf("pantalla1", "pantalla2", "pantalla3", "pantalla4")
 
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 150.dp, start = 8.dp, end = 8.dp, bottom = 8.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        itemsIndexed(images) { index, imageRes ->
+            Box(
+                modifier = Modifier
+                    .size(140.dp)
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(Color.LightGray)
+                    .clickable {
+                        navController.navigate(routes[index])
+                    },
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(id = imageRes),
+                    contentDescription = null,
+                    modifier = Modifier.size(100.dp)
+                )
+            }
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+private fun Preivi() {
+    ArtistsGallery(navController = rememberNavController())
 }
